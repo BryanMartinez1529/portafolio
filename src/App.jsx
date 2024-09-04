8d704e2d1c579b3575a26f895eca263a0a166a46
BryanMartinez152 <bryanwladimir8@gmail.com>
2024-09-04 17:20:39 -0500

    Arreglo de perfil

--- src/App.jsx
+++ src/App.jsx
@@ -13,24 +13,36 @@ function App() {
           Bienvenidos
         </div>
       </div>
-      <div className="flex items-center justify-center w-full h-screen bg-shark-950 ">
-        <div className='w-5/6 h-4/6 bg-shark-950 flex items-center  text-white overflow-hidden border'>
-          <img src="/img/foto_personal.jpg" alt="Foto personal" className='w-40 h-40 rounded-xl' />
-          <p className='font-bold text-justify mt-4 px-4 '>
-            Soy un apasionado desarrollador web con experiencia en la creación de sitios web responsivos y funcionales. 
-            Tengo habilidades avanzadas en HTML, CSS, JavaScript, así como experiencia en el diseño y desarrollo de interfaces 
-            de usuario atractivas. He trabajado en proyectos de diversa envergadura, desde pequeñas empresas hasta grandes 
-            corporaciones, siempre enfocado en la excelencia y la satisfacción del cliente. Estoy constantemente aprendiendo 
-            nuevas tecnologías y tendencias para seguir creciendo profesionalmente en este campo en constante evolución.
-          </p>
+
+      <div className="flex items-center justify-center w-full h-screen bg-shark-950">
+        <div className="w-5/6 h-4/6 bg-shark-950 flex flex-col justify-between items-center  overflow-hidden  md:flex-row  ">
+          <div className="  h-full flex items-center justify-center flex-col w-full md:w-full ">
+            <img src="/img/foto_personal.jpg" alt="Foto personal" className="w-52 h-52 rounded-full  shadow-lg drop-shadow-2xl " />
+            <div className='flex flex-row justify-between'>
+              <img src="/src/assets/icons/link.svg" className="m-4 shadow-lg hover:bg-mantis-800 hover:pointer" />
+              <img src="/src/assets/icons/link.svg" className="m-4 shadow-lg hover:bg-mantis-800 hover:pointer" />
+              <img src="/src/assets/icons/link.svg" className="m-4 shadow-lg hover:bg-mantis-800 hover:pointer" />
+              <a className='hover:pointer'><img src="/src/assets/icons/link.svg" className="m-4 shadow-lg hover:bg-mantis-800 " /></a>
+            </div>
+          </div>
+          <div className="w-full md:w-full  h-full flex justify-center items-center  text-white">
+            <p className="font-bold text-justify px-4 text-sm md:text-base drop-shadow-2xl  ">
+              Soy un apasionado desarrollador web con experiencia en la creación de sitios web responsivos y funcionales. 
+              Tengo habilidades avanzadas en HTML, CSS, JavaScript, así como experiencia en el diseño y desarrollo de interfaces 
+              de usuario atractivas. He trabajado en proyectos de diversa envergadura, desde pequeñas empresas hasta grandes 
+              corporaciones, siempre enfocado en la excelencia y la satisfacción del cliente. Estoy constantemente aprendiendo 
+              nuevas tecnologías y tendencias para seguir creciendo profesionalmente en este campo en constante evolución.
+            </p>
+          </div>
         </div>
-      </div>
+    </div>
         
        
       
-      <div className='bg-bunker-950'>
-        <div className='flex-1'> 
-            <h1 className="font-bold text-white  uppercase">Proyectos</h1>        
+      <div className='bg-bunker-950 flex h-screen'>
+        <h1 className="font-bold text-white  uppercase text-center">Proyectos</h1>
+        <div className='flex-1 '> 
+           <h1 className="font-bold text-white  uppercase text-center">Proyectos</h1>
         </div>
         <div className='py-10  w-full relative   text-white flex-1 ' >
               <img src="../public/img/foto_personal.jpg" alt="" className='w-max h-40 rounded-full ' />
@@ -45,7 +57,7 @@ function App() {
                 nuevas tecnologías y tendencias para seguir creciendo
                 profesionalmente en este campo en constante evolución.
                 </p>
-          </div>
+        </div>
        
        
       </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className=' fixed  bg-shark-950  w-full top-0 text-white flex items-center justify-between px-4 lg:px-8 py-2'>
-        <h1 className='text-bold  hover:text-mantis-400 text-2xl '>Mi Portafolio</h1>
+    <div className='relative  bg-shark-950  w-full top-0 text-white flex items-center justify-between px-4 lg:px-8 py-2'>
+        <h1 className='text-bold  hover:text-mantis-400 text-2xl '>Bryan Martinez
+        </h1>
         <ul className='mx-auto flex items-center'> 
             <li className='py-3 p-5 xl:p-6'><a href='#' className='hover:pointer  hover:text-mantis-400 '>Inicio</a></li>
             <li className='py-3 p-5 xl:p-6 hover:bg-mantis-900'> Proyectos</li>
